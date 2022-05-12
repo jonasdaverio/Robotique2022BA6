76a5a9eaf04f9032f1c402a7ca001e057c55018b
Jonas Daverio <jonas.daverio@epfl.ch>
2022-05-12 16:35:49 +0200

    Added main conf files, checked for compilation

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "ch.h"
+#include "hal.h"
+#include "memory_protection.h"
+
+#include <main.h>
+
+
+int main(void)
+{
+	halInit();
+	chSysInit();
+	mpu_init();
+
+	return 0;
+}
+
+#define STACK_CHK_GUARD 0xe2dee396
+uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
+
+void __stack_chk_fail(void)
+{
+    chSysHalt("Stack smashing detected");
+}
